4c20ed802dc6fca6c3391fe69cfc0902d0b66c95
macbook <limpopolly@mail.ru>
2025-03-02 22:10:50 +0300

    hw4 fix

--- src/main/java/ru/otus/java/basic/homeworks/homework4/Box.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework4/Box.java
@@ -3,48 +3,38 @@ package ru.otus.java.basic.homeworks.homework4;
 public class Box {
     private String color;
     private int size;
-    private boolean condition;
-    private String item;
+
+    public Box(String color, int size, boolean condition) {
+        this.color = color;
+        this.size = size;
+    }
 
     public String getColor() {
         return color;
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    public boolean isCondition() {
-        return condition;
-    }
-
-    public String getItem() {
-        return item;
     }
 
     public void setColor(String color) {
         this.color = color;
     }
 
-    public void setSize(int size) {
+    public int getSize() {
+        return size;
+    }
+
+    private void setSize(int size) {
         this.size = size;
     }
 
-    public void setCondition(boolean condition) {
-        this.condition = condition;
-    }
-
-    public void setItem(String item) {
-        this.item = item;
-    }
-
-    public Box(String color, int size, boolean condition) {
-        this.color = color;
-        this.size = size;
-        this.condition = condition;
-        this.item = item;
-    }
     public void infoBox () {
-        System.out.println(color + " " + size + " " + condition + " " + item);
+        System.out.println(color + " " + size);
+    }
+
+    public boolean openBox() {
+        System.out.println("Коробка открыта");
+        return true;
+    }
+    public boolean closeBox() {
+        System.out.println("Коробка закрыта");
+        return false;
     }
 }
